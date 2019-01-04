47d1858afda10e2c6ef31518663a0bbbb86d857f
Pradyumna Nagendra <pradyumnan@ilimi.in>
2019-01-04 18:53:33 +0530

    Issue #SC-834 feat : Test cases

---
+++ test/common/CassandraTestSetup.java
@@ -0,0 +1,51 @@
+package common;
+
+import com.datastax.driver.core.Session;
+import org.cassandraunit.utils.EmbeddedCassandraServerHelper;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import play.test.WithApplication;
+import utils.CassandraConnector;
+
+public class CassandraTestSetup extends WithApplication {
+
+	private static Session session = null;
+
+	@AfterClass
+	public static void afterTest() throws Exception {
+		tearEmbeddedCassandraSetup();
+	}
+
+	@BeforeClass
+	public static void before() throws Exception {
+		setupEmbeddedCassandra();
+	}
+
+	protected static Session getSession() {
+		return session;
+	}
+
+	private static void setupEmbeddedCassandra() throws Exception {
+		EmbeddedCassandraServerHelper.startEmbeddedCassandra("/cassandra-unit.yaml", 100000L);
+		session = CassandraConnector.getSession();
+	}
+
+	private static void tearEmbeddedCassandraSetup() {
+		try {
+			if (null != session && !session.isClosed())
+				session.close();
+			EmbeddedCassandraServerHelper.cleanEmbeddedCassandra();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	protected static void executeScript(String... querys) throws Exception {
+		session = CassandraConnector.getSession();
+		for (String query : querys) {
+			session.execute(query);
+		}
+	}
+
+}
