076bddaf700735931c319eb0c72af8de74b076ca
Kumar Gauraw <er.krgauraw@gmail.com>
2021-02-04 11:50:44 +0530

    Issue #SC-2086 feat: script changes for dockerization and java upgrade

--- vmsetup.sh
+++ vmsetup.sh
@@ -8,4 +8,8 @@ sudo service elasticsearch status
 find ./ -type f -name "logback.xml" -print0 | xargs -0 sed -i -e 's/\/data\/logs/logs/g'
 find ./ -type f -name "application.conf" -print0 | xargs -0 sed -i -e 's/\/data\//~\//g'
 find ./ -type f -name "*.java" -print0 | xargs -0 sed -i -e 's/\/data\//~\//g'
-mvn clean install -DskipTests
+java -version
+mvn -v
+mvn scoverage:report
+JAVA_REPORT_PATHS=`find /home/circleci/project  -iname jacoco.xml | awk 'BEGIN { RS = "" ; FS = "\n"; OFS = ","}{$1=$1; print $0}'`
+mvn verify sonar:sonar -Dsonar.projectKey=project-sunbird_sunbird-dial-service -Dsonar.organization=project-sunbird -Dsonar.host.url=https://sonarcloud.io -Dsonar.coverage.jacoco.xmlReportPaths=${JAVA_REPORT_PATHS}
