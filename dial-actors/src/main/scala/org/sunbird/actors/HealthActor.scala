9e9855a281371a489e85270f0bf2ca9e46dcb8f7
aimansharief <aiman.sharief.17@gmail.com>
2024-04-02 12:01:26 +0530

    Issue #KN-960 fix: Refactoring Sunbird Dial Service

---
+++ dial-actors/src/main/scala/org/sunbird/actors/HealthActor.scala
@@ -0,0 +1,20 @@
+package org.sunbird.actors
+
+import javax.inject.{Inject, Named}
+import scala.concurrent.{ExecutionContext, Future}
+import org.sunbird.actor.core.BaseActor
+import org.sunbird.commons.dto.{Request, Response}
+import org.sunbird.managers.HealthCheckManager
+
+@Named("healthActor")
+class HealthActor @Inject() extends BaseActor {
+
+  implicit val ec: ExecutionContext = getContext().dispatcher
+
+  @throws[Throwable]
+  override def onReceive(request: Request): Future[Response] = {
+    Future {
+      new HealthCheckManager().getAllServiceHealth
+    }
+  }
+}
---
+++ dial-core/src/main/java/org/sunbird/commons/AppConfig.java
@@ -0,0 +1,37 @@
+package org.sunbird.commons;
+
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 
+ * @author Pradyumna
+ *
+ */
+
+public class AppConfig {
+	private static Config defaultConf = ConfigFactory.load();
+	private static Config envConf = ConfigFactory.systemEnvironment();
+	public static Config config = defaultConf.withFallback(envConf);
+
+	public static String getString(String key, String defaultValue) {
+        return config.hasPath(key) ? config.getString(key) : defaultValue;
+    }
+
+    public static Double getDouble(String key, Double defaultValue) {
+        return config.hasPath(key) ? config.getDouble(key) : defaultValue;
+    }
+
+    public static Long getLong(String key, Long defaultValue) {
+        return config.hasPath(key) ? config.getLong(key) : defaultValue;
+    }
+
+	public static Boolean getBoolean(String key, Boolean defaultValue) {
+		return config.hasPath(key) ? config.getBoolean(key) : defaultValue;
+	}
+
+}
